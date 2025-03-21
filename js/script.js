45c23413de43eeec7c536eae3f88e12000aec51c
Feng Ouyang <fouyang@gmail.com>
2025-03-21 13:59:42 -0400

    Starting point: end of Section 7.2

---
+++ js/script.js
@@ -0,0 +1,5 @@
+  // script.js
+  document.addEventListener("DOMContentLoaded", () => {
+    console.log("App loaded");
+    // This is a symple test.
+  });
---
+++ js/sw.js
@@ -0,0 +1,33 @@
+const devMode = location.hostname === 'localhost';
+
+const CACHE_NAME = 'pwa-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/script.js',
+  '/mode1.html',
+  '/assets/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  if (devMode) {
+    console.log('[SW] Dev mode: skipping cache');
+    return;
+  }
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[SW] Caching app shell');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (devMode) return;
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
